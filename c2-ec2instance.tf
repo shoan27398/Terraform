11618d0a10ea60df5bc35809031d7b93abf2a137
Shoan <shoan.itx@gmail.com>
2024-01-09 13:12:50 +0000

    changes

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -1,8 +1,12 @@
 # Resource: EC2 Instance
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired AWS region
+}
+
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0533f2ba8a1995cf9"
+  ami           = "ami-03f4878755434977f"
   instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
+  user_data     = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
   }
