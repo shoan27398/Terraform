b3b77ea957d290ae846d6b1c60ddb70d60fe7e31
Shoan <shoan.itx@gmail.com>
2024-01-09 13:12:19 +0000

    changes

--- app1-install.sh
+++ app1-install.sh
@@ -1,10 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 # Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+
+# Update and install Apache
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
-sudo service httpd start  
-sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
-sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo service httpd start
+
+# Create HTML files
+echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
+sudo mkdir -p /var/www/html/app1
+echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+
+# Fetch and save instance metadata
 sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
